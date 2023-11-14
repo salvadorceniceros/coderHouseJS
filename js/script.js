3835650dcffa8d75768ea977949a4af806a8fcc5
salvadorceniceros <salvadorcs@MacBook-Pro-de-Salvador.local>
2023-11-14 01:28:25 -0600

    Ingresando cambios en los condicionales en JS

--- js/script.js
+++ js/script.js
@@ -21,21 +21,47 @@
 //     console.log("El color no es rojo");
 // }
 
-let precio = parseInt(prompt("Ingrese el precio: "));
+/* let precio = parseInt(prompt("Ingrese el precio: "));
 
-if(precio == ""){
-    alert("No ingresaste un precio valido")
-}
-else if(precio < 20)
+if (precio < 20)
 {
     alert("El precio es menor que 20");
 }
-else if(precio < 50){
+else if (precio < 50){
     alert("El precio es menor que 50")
 }
-else if(precio < 100){
+else if (precio < 100){
     alert("El precio es menor que 100")
-}else{
+}
+else if (precio > 100){
     alert("El precio es mayor que 100")
 }
-console.log("Listo!")
+else{
+    alert("No ingresaste un precio valido")
+}
+console.log("Listo!") 
+
+let nombreIngresado = prompt("Ingrese su nombre");
+let apellidoIngresado = prompt("Ingrese su apellido");
+
+if ((nombreIngresado != "") && (apellidoIngresado != "")){
+    alert("Nombre "+nombreIngresado+" y su appelido es: "+apellidoIngresado)
+}else{
+    alert("Error al ingresay nombre y apellido")
+}
+
+let nombre = prompt("Ingrese el nommbre: ")
+
+if((nombre == "ANA") || (nombre == "Ana")){
+    alert("El nombre ingresado es An4")
+}else{
+    alert("El nombre ingresado no es Ana")
+}*/
+
+let nombreIngresado = prompt("Ingrese nombre:")
+
+if((nombreIngresado != "") && ((nombreIngresado == "ANA") || (nombreIngresado == "ana"))){
+    alert("Hola Ana")
+}else{
+    alert("Ingresa un nombre vlalido")
+}
